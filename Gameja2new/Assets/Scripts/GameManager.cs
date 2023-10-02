22a59184dc112af60313d774759de43bb488c904
Samuel Eduardo Álvarez Sepúlveda <71793961+SeasterBlue@users.noreply.github.com>
2023-10-02 15:43:58 -0500

    Small Update and fixed 7 and Vignette

--- Gameja2new/Assets/Scripts/GameManager.cs
+++ Gameja2new/Assets/Scripts/GameManager.cs
@@ -8,6 +8,12 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject gameOverUI;
+    private GameObject gun;
+
+    private void Start()
+    {
+        gun = GameObject.Find("GunFinal");
+    }
 
     public bool isGameActive = true;
     //public int zombieToMedicine = 5;
@@ -17,12 +23,18 @@ public class GameManager : MonoBehaviour
     {
         gameOverUI.gameObject.SetActive(true);
         isGameActive = false;
+        gun.SetActive(false);
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void Epilogue()
+    {
+        SceneManager.LoadScene("Ending");
+    } 
 
     //public void AddZombieLoopCount()
     //{
